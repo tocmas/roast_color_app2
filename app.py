8be52757d1f6266d3d48a166a8e4abfb24c15fbd
tocmas <tocmas.m@gmail.com>
2023-05-23 20:37:55 +0900

    1st commit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(["png", "jpg", "gif", "jpeg"])
 
 def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENTIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET", "POST"])
 def predicts():
