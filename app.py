f24d353ef518ed4aa713b48d75aaaabd823140b0
tocmas <tocmas.m@gmail.com>
2023-05-23 20:05:34 +0900

    1st commit

--- app.py
+++ app.py
@@ -14,41 +14,16 @@ def predict(img):
     return y
 
 def getName(label):
-    # ラベルに対応する名称を返すための関数を15クラスに対応するように修正
-    if label == 0:
-        return "Class 1"
-    elif label == 1:
-        return "Class 2"
-    elif label == 2:
-        return "Class 3"
-    elif label == 3:
-        return "Class 4"
-    elif label == 4:
-        return "Class 5"
-    elif label == 5:
-        return "Class 6"
-    elif label == 6:
-        return "Class 7"
-    elif label == 7:
-        return "Class 8"
-    elif label == 8:
-        return "Class 9"
-    elif label == 9:
-        return "Class 10"
-    elif label == 10:
-        return "Class 11"
-    elif label == 11:
-        return "Class 12"
-    elif label == 12:
-        return "Class 13"
-    elif label == 13:
-        return "Class 14"  
-    elif label == 14:
-        return "Class 15"
+    class_names = [
+        "Class 1", "Class 2", "Class 3", "Class 4", "Class 5",
+        "Class 6", "Class 7", "Class 8", "Class 9", "Class 10",
+        "Class 11", "Class 12", "Class 13", "Class 14", "Class 15"
+    ]
+    return class_names[label]
     
 app = Flask(__name__)
 
-ALLOWED_EXTENTIONS = set(["png", "jpg", "gif", "jpeg"])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "gif", "jpeg"])
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENTIONS
@@ -66,7 +41,7 @@ def predicts():
             image = Image.open(file)
             image.save(buf, "png")
             base64_str = base64.b64encode(buf.getvalue()).decode("utf-8")
-            base64_data = "data:image/png;base64, {}".format(base64_str)
+            base64_data = "data:image/png;base64,{}".format(base64_str)
             pred = predict(image)
             colorName_= getName(pred)
             return render_template("result.html", colorName=colorName_, image = base64_data)
